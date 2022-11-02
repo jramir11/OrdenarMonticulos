8a6d8e3684e4bc3222e538d0f2d2ca12e0bf07f9
Javier Ramirez <jramir11@bancoestado.cl>
2022-11-02 17:27:12 -0300

    ok con heapify

--- ordenarMonticulos.js
+++ ordenarMonticulos.js
@@ -1,39 +1,10 @@
-//salida 1 : Usa un montículo mínimo para crear un array completamente ordenado.
-/*
-function ordenarHeap (heap)
-{
-    let p=0;
-    let j=0;
-    let tmp=[];
-    
-    for (p=1;p<heap.length;p++) {
-        tmp = heap[p];
-        j = p - 1;
-
-        while (heap[j] > tmp && j >= 0) {
-            heap[j+1] = heap[j];
-            j--;
-        }
-        heap[j+1] = tmp;
-    }
-
-
-    //para mostrar como queda el array heap
-    console.log(heap);
-}
-ordenarHeap([3, 8, 10, 11, 9, 20, 14]); // deberia retornar 3,8,9,10,11,14,20
-*/
-
-
-
-
-//salida 1 : crear un array nuevo usando ordenar y hapify como funciones en un array nuevo
+//salida  : crear un array nuevo usando ordenar y hapify como funciones en un array nuevo
 function nuevoHeap (heap)
 {
     let nuevoHeap =[];
     let nuevoHeapFinal=[];
 
-    nuevoHeap = hapifyHeap(heap);  //convierte en monticulo
+    nuevoHeap = heapify(heap);  //convierte en monticulo
 
     nuevoHeapFinal = insertarNuevoHeap(nuevoHeap);  //ordena y crea NuevoHeapFinal
 
@@ -41,59 +12,69 @@ function nuevoHeap (heap)
 }
 
 
-//insertar
+
+//hapify crea un monticulo con el arreglo inicial
+function heapify(element){
+    //crear monticulo inicial
+    let temp=element[0];
+    element[0]=undefined;
+    element.push(temp);
+
+    let nuevoHeap=[];
+    for (let i = 0; i < element.length; i++) {
+        nuevoHeap.push(element[i]);
+    }
+
+   
+    //heapify de nuevo monticulo
+    let j = nuevoHeap.length - 1;
+    let idx_padre = Math.trunc(j / 2);
+    let idx_hijoMenor = idx_padre * 2;
+    let idx_hijoMayor = (idx_padre * 2) + 1;
+
+    while (idx_padre > 0) {
+        temp = nuevoHeap[idx_padre];
+
+        if (nuevoHeap[idx_padre] > nuevoHeap[idx_hijoMayor]) {
+                nuevoHeap[idx_padre] = nuevoHeap[idx_hijoMayor];
+                nuevoHeap[idx_hijoMayor] = temp;
+        }
+        if (nuevoHeap[idx_padre] > nuevoHeap[idx_hijoMenor]) {
+                nuevoHeap[idx_padre] = nuevoHeap[idx_hijoMenor];
+                nuevoHeap[idx_hijoMenor] = temp;
+        } 
+        idx_padre = idx_padre - 1;
+        idx_hijoMenor = idx_padre * 2;
+        idx_hijoMayor = (idx_padre * 2) + 1;
+    }
+
+    return nuevoHeap;
+}
+
+
+
+//insertar y ordenar monticulo
 function insertarNuevoHeap(element){
     let indice_hijo = 0;
     let temp=0;
     let nuevoHeap=[];
-
     for(let i=0;i<element.length;i++){
         //insertando el nuevo valor en el array heap
         nuevoHeap.push(element[i]);
         //indice del nuevo elemento
         indice_hijo = nuevoHeap.length - 1;
-        indice_padre = indice_hijo - 1;//Math.trunc(indice_hijo / 2);
-
+        indice_padre = indice_hijo - 1;
         //compara padre e hijo e invierte valores si padre es mayor
         while (nuevoHeap[indice_padre] > nuevoHeap[indice_hijo]){
             temp = nuevoHeap[indice_hijo];
             nuevoHeap[indice_hijo] = nuevoHeap[indice_padre];
             nuevoHeap[indice_padre] = temp;
-
             indice_hijo=indice_padre;
-            indice_padre = indice_hijo - 1;//Math.trunc(indice_hijo / 2);
+            indice_padre = indice_hijo - 1;
         }
     }
     return nuevoHeap;
 }
 
-
-//hapify crea un monticulo con el arreglo inicial
-function hapifyHeap(element) {
-    let indice_hijo = 0;
-    let temp=0;
-    let nuevoHeap=[];
-    nuevoHeap.push(undefined);
-
-    for(let i=0;i<element.length;i++){
-
-        //insertando el nuevo valor en el array heap
-        nuevoHeap.push(element[i]);
-        //indice del nuevo elemento
-        indice_hijo = nuevoHeap.length - 1;
-        indice_padre = Math.trunc(indice_hijo / 2);
-
-        //compara padre e hijo e invierte valores si padre es mayor
-        while (nuevoHeap[indice_padre] > nuevoHeap[indice_hijo]){
-            temp = nuevoHeap[indice_hijo];
-            nuevoHeap[indice_hijo] = nuevoHeap[indice_padre];
-            nuevoHeap[indice_padre] = temp;
-
-            indice_hijo=indice_padre;
-            indice_padre = Math.trunc(indice_hijo / 2);
-        }
-    }
-    return nuevoHeap;
-}
 
 nuevoHeap([3, 8, 10, 11, 9, 20, 14]); // deberia retornar 3,8,9,10,11,14,20 
